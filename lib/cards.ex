68e1b20591f70c5e32b9dd46bd2c25cdfd849345
Steven C <stevencch99@gmail.com>
2021-06-14 18:36:55 +0800

    Add shuffle function

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,4 +2,9 @@ defmodule Cards do
   def create_deck do
     ["Ace", "Two", "Three"]
   end
+
+  # make a list copy and then return the shuffled list
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 end
